17beda53c4ed408684139ea7b728e1a4f9193e61
Shailesh Kumar <skbhagat40@gmail.com>
2020-10-09 13:57:48 +0530

    Update partition_string.py

--- DSA_NOTES/two_pointers/partition_string.py
+++ DSA_NOTES/two_pointers/partition_string.py
@@ -2,3 +2,15 @@
 String split, such that in each part only one char occurs.
 use single, traversal, if cross the rightmost occurance, update it
 """
+# Implementation
+
+def partition_string(S):
+  right_most = {c: i for i, c in enumerate(S)}
+  left_pos = 0
+  ans = []
+  for i in range(len(S)):
+    char = S[i]
+    if right_most[char] == i:
+      ans.append(i-left_pos)
+      left_pos = i + 1
+  return ans
